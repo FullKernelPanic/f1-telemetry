202c51a4743251e60af5bacf3c721a1062529791
FullKernelPanic <zackraven@gmail.com>
2024-04-26 19:38:17 +0200

    chore: delete client

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # f1-telemetry
+
+## Server
+On Windows dev build (git bash)
+````
+cd server
+go build main.go && ./main.exe
+````
+
+## Client
+
+````
+cd client
+npm run watch
+````
--- client/src/charts/charts.ts
+++
@@ -1,7 +0,0 @@
-import { Chart, registerables as chartRegistables } from "chart.js";
-import registerables from "chartjs-plugin-streaming";
-
-export default function() {
-    Chart.register(...chartRegistables)
-    Chart.register(...registerables)
-}
